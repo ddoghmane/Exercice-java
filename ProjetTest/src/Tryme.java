83e38db253056883a22659d754a40a3acda44cb4
ddoghmane <58287184+ddoghmane@users.noreply.github.com>
2019-12-17 10:12:43 +0100

    add javadoc

---
+++ ProjetTest/src/Tryme.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author CRM
+ *
+ */
+public class Tryme {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
